bcd09f4bc96015ee41772b4a1438bffea1739d3e
Thomas Hofmann <thomas@savoy-truffle.de>
2021-06-16 13:31:33 +0200

    added implementation of GET /messages/:id

--- data-server/03_mock-db_and_mvc/live/20-server/server.js
+++ data-server/03_mock-db_and_mvc/live/20-server/server.js
@@ -11,6 +11,7 @@ const adapter = new JSONFile('db.json');
 const db = new Low(adapter);
 
 await db.read();
+// "||=" available since node v15.0
 db.data ||= { messages: [] };
 
 app.listen(3000, () => {
@@ -18,8 +19,14 @@ app.listen(3000, () => {
 });
 
 // http://localhost:3000/messages/1
+// http://localhost:3000/messages/2
 app.get('/messages/:id', async (req, res) => {
-	// TODO: sende Response mit der Nachricht (ID) als json zum Client.
+	// TODO: sende als Response die Nachricht (message) 
+	//       deren ID mit dem Parameter (:id) übereinstimmt
+	//       als json zum Client.
+	const id = req.params.id;
+	const message = db.data.messages.find(message => message.id === id)
+	res.status("200").send(message);
 })
 
 app.post('/messages', async (req, res) => {
